7d8676ae4e14590ea5f0cda7aaa5508d9e620ed8
Aarthi Sundaram <aarthy.sundaram@alumni.ashoka.edu.in>
2020-07-13 17:40:18 +0200

    ndvi update

--- NDVI_Jharkhand.js
+++ NDVI_Jharkhand.js
@@ -1,20 +1,22 @@
+//IMPORT TABLE jharkhand shapefile table
 var dataset = ee.ImageCollection('LANDSAT/LC08/C01/T1_ANNUAL_NDVI')
-                  .filterDate('2012-01-01', '2017-12-31');
+.filterDate('2012-01-01', '2017-12-31');
 var colorized = dataset.select('NDVI');
 var colorizedVis = {
-  min: 0.0,
-  max: 1.0,
-  palette: [
-    'FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901',
-    '66A000', '529400', '3E8601', '207401', '056201', '004C00', '023B01',
-    '012E01', '011D01', '011301'
-  ],
+min: 0.0,
+max: 1.0,
+palette: [
+'FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901',
+'66A000', '529400', '3E8601', '207401', '056201', '004C00', '023B01',
+'012E01', '011D01', '011301'
+],
 };
 
 //Longitude: 85.31087372191023
-​//Latitude:  23.326981621613303
-​//Zoom Level: 8
-​​//Scale (approx. m/px): 1222.99245256282
+//Latitude:  23.326981621613303
+//Zoom Level: 8
+//Scale (approx. m/px): 1222.99245256282
 
 Map.setCenter(85.31087372191023, 23.326981621613303, 8);
 Map.addLayer(colorized, colorizedVis, 'Colorized');
+Map.addLayer(table);  //import table. 
