0db1984ccae2509d30efd0daf16a75d86697dce6
Aarthi Sundaram <aarthy.sundaram@alumni.ashoka.edu.in>
2020-07-14 22:36:34 +0200

    jharkhand_NDVI w/o exporting yet

--- NDVI_Jharkhand.js
+++ NDVI_Jharkhand.js
@@ -1,6 +1,3 @@
-//import jharkhand state boundary shapefile from Assets 
-//obtained Jharkhand state boundary using QGIS. 
-
 var dataset = ee.ImageCollection('LANDSAT/LC08/C01/T1_ANNUAL_NDVI')
                   .filterDate('2012-01-01', '2017-12-31')
                   .filterBounds(jharkhand)
@@ -16,6 +13,14 @@ var colorizedVis = {
     '012E01', '011D01', '011301'
   ],
 };
+// create function to crop with state boundaries
+var clipping_func = function(image) {
+  // Crop by table extension
+  return image.clip(jharkhand);
+};
+
+//clip the NDVI images to jharkhand's boundaries
+var NDVIClipped = colorized.map(clipping_func);
 
 //Longitude: 85.31087372191023
 //Latitude:  23.326981621613303
@@ -23,6 +28,6 @@ var colorizedVis = {
 //Scale (approx. m/px): 1222.99245256282
 
 Map.setCenter(85.31087372191023, 23.326981621613303, 8);
-Map.addLayer(colorized, colorizedVis, 'Colorized');
-Map.addLayer(jharkhand);
+Map.addLayer(jharkhand)
+Map.addLayer(NDVIClipped, colorizedVis, 'Colorized');
 
